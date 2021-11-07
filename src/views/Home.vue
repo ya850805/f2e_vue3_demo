57ca6fa940be10cb04df60c97dda5af034009290
Jason <ya850805@gmail.com>
2021-11-07 22:43:17 +0800

    refactor: Delete the common page(Search.vue), move it to Home page.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="home">
     <h1>This is an home page</h1>
-    <Search></Search>
+    <div class="searchArea">
+      <h2>搜尋</h2>
+      <div>
+        <select>
+          <option>台北市</option>
+        </select>
+      </div>
+      <div>
+        <input type="text" placeholder="請輸入路線、站名"/>
+      </div>
+    </div>
 
     <div>
       <router-link to="/home/routes">Routes</router-link>
@@ -16,12 +26,18 @@
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import Search from "@/components/common/Search.vue";
 
 @Options({
   components: {
-    Search
   },
 })
 export default class Home extends Vue {}
 </script>
+
+<style>
+.searchArea {
+  width: 25%;
+  border: 2px red solid;
+  border-radius: 20px;
+}
+</style>
